2b43e04002dd891b9d4a7ecb40509370088af172
Arevik Keshishyan <arevik.keshishyan@SIS2N060-4.local>
2025-06-15 15:36:53 +0400

    Commit two

--- conftest.py
+++ conftest.py
@@ -3,12 +3,15 @@ import pytest
 import logging
 import os
 from datetime import datetime
-# import allure
+import allure
 
 
 @pytest.fixture()
 def test_driver():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
     driver.quit()
@@ -34,13 +37,13 @@ def test_logger(request):
     logger.info(f'{test_name} is finished')
 
 
-# @pytest.hookimpl(hookwrapper=True)
-# def pytest_runtest_makereport(item):
-#     outcome = yield
-#     result = outcome.get_result()
-#     if result.when == "call":
-#         if result.outcome == 'failed':
-#             allure.attach(
-#                 item.funcargs.get("test_driver").get_screenshot_as_png(),
-#                 name=f"{item.name}_screen",
-#                 attachment_type=allure.attachment_type.PNG)
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    result = outcome.get_result()
+    if result.when == "call":
+        if result.outcome == 'failed':
+            allure.attach(
+                item.funcargs.get("test_driver").get_screenshot_as_png(),
+                name=f"{item.name}_screen",
+                attachment_type=allure.attachment_type.PNG)
